c7514d4573e84fe78716e228fa4a7d96f9149b8e
Shivansh Soni <shivanshsoni568@gmail.com>
2024-06-04 21:42:09 +0530

    Map Put

---
+++ Day 13/Map/src/Mapput.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Mapput {
+    public static void main(String[] args) {
+        // Create a HashMap
+        var map = new HashMap<String, Integer>();
+
+        // Insert key-value pairs into the map using the put() method
+        map.put("Prateek", 1);
+        map.put("Ridhaan", 2);
+        map.put("Shlok", 3);
+
+        // Print the map contents
+        System.out.println("Original map: " + map);
+
+        // Update the value associated with the key "Banana"
+        map.put("Sujal", 4);
+
+        // Add a new key-value pair
+        map.put("Tarun", 5);
+
+        // Print the map contents after updates
+        System.out.println("Updated map: " + map);
+    }
+}
